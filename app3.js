0c2f5fa39b62de88d2b3db0319882a193ad2ae02
hieukmv <49061281+hieukmv@users.noreply.github.com>
2019-03-30 22:46:29 +0700

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(window).scroll(function () { 
+        if(window.pageXOffset >= 200){
+            $("header").addClass("thunho");
+        } else{
+
+        }
+    });
+});
+
